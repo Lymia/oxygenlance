e6ac2cad58624d470c5f8e438c1e2e9668c0dd61
Rimi Kanokawa <rimi@rimin.moe>
2024-10-23 03:22:23 -0700

    Improve the oxygenlance CLI.

--- examples/oxygenlance-cli.rs
+++ examples/oxygenlance-cli.rs
@@ -1,12 +1,17 @@
 use anyhow::{Error, Result};
 use clap::Parser;
 use oxygenlance::{RoundResult, Warrior, MATCH_COUNT};
+use rayon::prelude::*;
 use std::{fmt::Write, path::PathBuf};
 
 /// A simple reimplementation of gearlance.
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
+    /// Run every matchup, and in a very predictable format. This is used to test oxygenlance.
+    #[arg(long)]
+    reference_mode: bool,
+
     /// The list of BF Joust warriors to run.
     warriors: Vec<PathBuf>,
 }
@@ -22,25 +27,42 @@ fn main() -> Result<()> {
     for warrior in args.warriors {
         eprintln!("Compiling {}...", warrior.display());
         let compiled = Warrior::compile(std::fs::read(&warrior)?)?;
-        warriors.push((warrior, compiled));
+        warriors.push((warrior.file_name().unwrap().to_string_lossy().to_string(), compiled));
     }
 
-    for (_, warrior_a) in &warriors {
-        for (_, warrior_b) in &warriors {
-            let result = warrior_a.run_match(warrior_b);
-            let mut line = String::new();
-            for configuration in 0..2 {
-                for i in 0..MATCH_COUNT {
-                    line.push(match result.results[configuration][i] {
-                        RoundResult::LeftWins => '<',
-                        RoundResult::RightWins => '>',
-                        RoundResult::Tie => 'X',
-                    });
-                }
-                line.push(' ');
+    let max_name = warriors.iter().map(|x| x.0.len()).max().unwrap();
+
+    let results: Vec<_> = warriors
+        .par_iter()
+        .enumerate()
+        .flat_map(|(i, (wa_name, wa))| {
+            let target = if args.reference_mode {
+                &warriors
+            } else {
+                &warriors[i + 1..]
+            };
+            target
+                .par_iter()
+                .map(|(wb_name, wb)| (wa_name.clone(), wb_name.clone(), wa.run_match(wb)))
+        })
+        .collect();
+    for (wa_name, wb_name, result) in results {
+        let mut line = String::new();
+        for configuration in 0..2 {
+            for i in 0..MATCH_COUNT {
+                line.push(match result.results[configuration][i] {
+                    RoundResult::LeftWins => '<',
+                    RoundResult::RightWins => '>',
+                    RoundResult::Tie => 'X',
+                });
             }
-            write!(line, "{}", result.score())?;
+            line.push(' ');
+        }
+        write!(line, "{}", result.score())?;
+        if args.reference_mode {
             println!("{line}");
+        } else {
+            println!("{wa_name:width$   } vs {wb_name:width$}: {line}", width = max_name);
         }
     }
 
