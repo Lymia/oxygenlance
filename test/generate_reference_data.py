3fd78aacdfe2446f13578c3933e0608714a4099a
Rimi Kanokawa <rimi@rimin.moe>
2024-10-23 00:20:48 -0700

    Modify test scripts to not use... whatever framework was in use beforehand.

--- test/generate_reference_data.py
+++ test/generate_reference_data.py
@@ -1,14 +1,22 @@
+#! /usr/bin/env nix-shell
+#! nix-shell -i python3 -p python3 -p nodejs
+
 import subprocess
 import sys
+import os
 
-from test import source
-from tools import runfiles
+dir_path = os.path.dirname(os.path.realpath(__file__))
+os.chdir(dir_path)
+
+source_order = list(filter(lambda x: len(x) != 0, open("warriors.idx").read().split("\n")))
 
 def main():
-    for left, right in source.pairs():
-        result = subprocess.check_output(
-            ['nodejs', runfiles.path('test/egojsout.js'), left.path, right.path])
-        sys.stdout.write(result)
+    with open("reference.data", "w") as f:
+        for left in source_order:
+            for right in source_order:
+                result = subprocess.check_output(['node', 'egojsout.js', left, right]).decode("latin1")
+                print(f"{left} vs {right}: {result.strip()}")
+                f.write(result)
 
 if __name__ == '__main__':
     main()
